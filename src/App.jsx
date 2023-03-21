262b1c68f03bbc1c9a10379a497064b752013628
Keira <keirasedacey@gmail.com>
2023-03-21 15:26:21 -0400

    refactored components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { Component } from "react";
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 // component imports
 // import { Login, Register } from './components/Login/index';
@@ -22,9 +21,13 @@ class App extends Component {
   render() {
     return (
       <div className='App' style={{ backgroundColor: 'white', width: '500px', height: '500px', border: '1px solid black' }}>  
+        {/* <Login
+        /> */}
 
         <TaskFeature 
           task={this.props.task}
+          
+
         />
       </div>
     )
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,21 +1,26 @@
-// import React, { Component } from "react";
+import React, { Component } from "react";
 
-// export class Login extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-    
-    
 
-//     render() {
-//         return <div>     
+export class Login extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+
+
+
+
+  render() {
+    return (
+        <div className='Login' style={{ backgroundColor: 'white', width: '500px', height: '500px', border: '1px solid black' }}>  
+          <label htmlFor="email">email</label>
+          <input type="email" placeholder="example@email.com" id="email"/>
+          <label htmlFor="password">password</label>
+          <input type="password" placeholder="*********" id="password"/>
+        </div>
+    )
+  }
+}      
                 
-//                 </div>
-//         }
-    
-    
-//     }
 
-// export default Login;
-
-
+export default Login;
